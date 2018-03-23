60ac31f92538e1a0eaaff0309f28b6a6d9375fa4
cinek5 <marcinkrawczyk97@hotmail.com>
2018-03-23 18:55:08 +0100

    validator refactor

--- src/main/resources/static/assets/js/moneyRequestForm.js
+++ src/main/resources/static/assets/js/moneyRequestForm.js
@@ -8,57 +8,70 @@ var app = new Vue({
         itemQuantity: 1,
         itemCategory: '',
         user: null,
-        errorCommunicate: ''
+        errorCommunicate: '',
+        error: false
 
 
     },
     methods: {
-        validateSubmit: function() {
+        validateSubmit: function () {
             this.errorCommunicate = '';
-            var error = false;
-                if (this.user==null) {
-                    error = true;
-                    this.errorCommunicate += 'You have to choose the user <br>';
-                }
-                if (this.items.length==0) {
-                    error = true;
-                    this.errorCommunicate += 'You have to add some items <br>';
-                }
-            return !error;
+            this.error = false;
+            this.validateUser();
+            this.validateItemsLength();
+            return !this.error;
         },
-        checkItem: function() {
-            var error = false;
-            var message = '';
-            if (this.itemPrice < 0) {
-                message+='Item price must be at least 0  <br>';
-                error = true;
-                this.itemPrice=0;
-            }
-            if (this.itemName=='') {
-                message+='Item name must not be at blank  <br>';
-                error = true;
+        setErrorAppendMessage: function (message) {
+            this.errorCommunicate += message;
+            this.error = true;
+        },
+        checkItem: function () {
+            this.error = false;
+            this.errorCommunicate = '';
+            this.validateItemName();
+            this.validateItemCategory();
+            this.validateItemPrice();
+            this.validateItemQuantity();
+            return !this.error;
+        },
+        validateItemName: function () {
+            if (this.itemName == '') {
+                this.setErrorAppendMessage('Item name must not be at blank  <br>');
 
+            } else if (!this.itemName.match(/[A-Z][a-z]*$/)) {
+                this.setErrorAppendMessage('Item name contains only letters and starts with capital <br>')
             }
+        },
+        validateUser: function() {
+            if (this.user == null) {
+                this.setErrorAppendMessage('You have to choose the user <br>');
+            }
+        },
+        validateItemCategory: function () {
+            if (this.itemCategory == '') {
+                this.setErrorAppendMessage('Item category must not be blank <br>');
+            } else if (!this.itemCategory.match(/[A-Z][a-z]*$/)) {
+                this.setErrorAppendMessage('Item category contains only letters and starts with capital <br>')
+            }
+        },
+        validateItemQuantity: function () {
             if (this.itemQuantity < 1) {
-                message+='Item quantity must be at least 1 <br>';
-                error = true;
-                this.itemQuantity=1;
+                this.setErrorAppendMessage('Item quantity must be at least 1 <br>');
+                this.itemQuantity = 1;
             }
-            if (this.itemCategory=='') {
-                message+='Item category must not be blank <br>';
-                error = true;
-            }
-            if (this.items.length==0) {
-                message+='You have to add at least 1 item <br>';
-            }
-            if (error) {
-               this.errorCommunicate = message;
-            }
-
-
-            return !error;
         },
-        addItem: function() {
+        validateItemsLength: function () {
+            if (this.items.length == 0) {
+                this.setErrorAppendMessage('You have to add at least 1 item <br>');
+            }
+        },
+        validateItemPrice: function () {
+            if (this.itemPrice < 0) {
+                this.setErrorAppendMessage('Item price must be at least 0  <br>');
+                this.itemPrice = 0;
+            }
+        },
+        addItem: function () {
             if (this.checkItem()) {
                 this.items.push({
                     name: this.itemName,
@@ -68,29 +81,29 @@ var app = new Vue({
                 });
             }
         },
-        postAddRequest: function() {
-            if (getCookie('access_token')!=null && this.validateSubmit()) {
-                axios.post("/moneyrequests/?access_token="+getCookie("access_token"),{
+        postAddRequest: function () {
+            if (getCookie('access_token') != null && this.validateSubmit()) {
+                axios.post("/moneyrequests/?access_token=" + getCookie("access_token"), {
                     requestedUser: this.user,
                     items: this.items,
                     status: "REQUESTED"
-                }).then(function() {
-                    document.location.replace("/userPage?access_token="+getCookie("access_token"));
+                }).then(function () {
+                    document.location.replace("/userPage?access_token=" + getCookie("access_token"));
                 })
-                    .catch(function(error) {
+                    .catch(function (error) {
                         console.log('problem with post request (add money request obj)');
                         console.log(error);
                     });
             }
         }
     },
-    mounted: function(){
+    mounted: function () {
 
-        if (getCookie('access_token')!=null) {
-            axios.get("/otherusers?access_token="+getCookie('access_token'))
-                .then((function(response){
+        if (getCookie('access_token') != null) {
+            axios.get("/otherusers?access_token=" + getCookie('access_token'))
+                .then((function (response) {
                     this.users = response.data;
-                }).bind(this)).catch(function(error) {
+                }).bind(this)).catch(function (error) {
                 console.log("cos sie zjebalo");
             });
 
@@ -99,6 +112,7 @@ var app = new Vue({
     }
 
 
-});/**
+});
+/**
  * Created by Cinek on 23.12.2017.
  */
